bc1fc43f0be251d5bed246a1863715a2fcbf7388
mtlem <mtlemlothar@gmail.com>
2024-05-06 13:08:24 -0300

    cadastro_Completo

--- world-building/src/firebaseDatabase.js
+++ world-building/src/firebaseDatabase.js
@@ -1,6 +1,7 @@
-//import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
 
-const firebaseConfigWorld = {
+const firebaseConfigDatabase = {
   apiKey: "AIzaSyB81OkcBZrWGfqW2BPpgbnLjUP_hwPH0Oc",
   authDomain: "world-building-6b582.firebaseapp.com",
   databaseURL: "https://world-building-6b582-default-rtdb.firebaseio.com",
@@ -8,6 +9,9 @@ const firebaseConfigWorld = {
   storageBucket: "world-building-6b582.appspot.com",
   messagingSenderId: "752971778873",
   appId: "1:752971778873:web:5d960848cff2eb780f36e8"
-  };
-  
-  export default firebaseConfigWorld;
+};
+
+const firebaseAppDatabase = initializeApp(firebaseConfigDatabase, 'database');
+const database = getDatabase(firebaseAppDatabase);
+
+export { database };
--- world-building/src/pages/user/CreateWorld.jsx
+++ world-building/src/pages/user/CreateWorld.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getDatabase, ref, push, set } from 'firebase/database';
 import './userCss/createWorld.css';
 import firebaseConfigWorld from '../../firebaseWorld';
@@ -8,7 +8,34 @@ const CreateWorld = () => {
   const [successMessage, setSuccessMessage] = useState('');
   const [creatorName, setCreatorName] = useState('Matheus');
   const [systemName, setSystemName] = useState('D&D');
+  const [notes, setNotes] = useState('');
+  const [editingWorld, setEditingWorld] = useState(null); // Estado para armazenar o ID do mundo sendo editado
   const db = getDatabase();
+
+  // Função para carregar os dados do mundo atualmente em edição, se houver
+  useEffect(() => {
+    if (editingWorld) {
+      const worldRef = ref(db, `mundo/${editingWorld}`);
+      // Obtém os dados do mundo atual e define os valores nos estados
+      const fetchData = async () => {
+        try {
+          const snapshot = await worldRef.get();
+          if (snapshot.exists()) {
+            const worldData = snapshot.val();
+            setWorldName(worldData.name);
+            setCreatorName(worldData.criador);
+            setSystemName(worldData.sistema);
+            setNotes(worldData.notes);
+          } else {
+            console.log("No data available");
+          }
+        } catch (error) {
+          console.error("Error getting document:", error);
+        }
+      };
+      fetchData();
+    }
+  }, [editingWorld, db]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,7 +46,7 @@ const CreateWorld = () => {
     // Agora, você pode definir os dados usando a chave
     set(ref(db, `mundo/${worldKey}`), {
       name: worldName,
-      notes: '',
+      notes: notes,
       criador: creatorName,
       sistema: systemName
     }).then(() => {
@@ -30,13 +57,17 @@ const CreateWorld = () => {
       console.log('Sistema:', systemName);
       // Define a mensagem de sucesso no estado
       setSuccessMessage('Mundo criado com sucesso!');
+      // Limpa os campos do formulário após o envio
+      setWorldName('');
+      setCreatorName('');
+      setSystemName('');
+      setNotes('');
     }).catch((error) => {
       // Se ocorrer um erro durante a criação do mundo, exibe o erro no console
       console.error('Erro ao criar mundo:', error);
       // Define a mensagem de erro no estado
       setSuccessMessage('Erro ao criar mundo. Verifique o console para mais detalhes.');
     });
-    setWorldName('');
   };
 
   return (
@@ -50,6 +81,29 @@ const CreateWorld = () => {
             type="text"
             value={worldName}
             onChange={(e) => setWorldName(e.target.value)}
+          />
+        </label>
+        <label>
+          Nome do Criador:
+          <input
+            type="text"
+            value={creatorName}
+            onChange={(e) => setCreatorName(e.target.value)}
+          />
+        </label>
+        <label>
+          Sistema:
+          <input
+            type="text"
+            value={systemName}
+            onChange={(e) => setSystemName(e.target.value)}
+          />
+        </label>
+        <label>
+          Notas:
+          <textarea
+            value={notes}
+            onChange={(e) => setNotes(e.target.value)}
           />
         </label>
         <button type="submit">Criar Mundo</button>
